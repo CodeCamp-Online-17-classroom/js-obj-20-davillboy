07f8ef32951414d618308bac136a94d087a49ab2
davillboy <71083807+davillboy@users.noreply.github.com>
2024-04-22 18:57:52 +0700

    first change

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // write code here
+<script>
+        let calculator = {
+            read() {
+              this.a = +prompt('Enter the first number:', 0);
+              this.b = +prompt('Enter the second number:', 0);
+            },
+            sum() {
+              return this.a + this.b;
+            },
+            mul() {
+              return this.a * this.b;
+            }
+          };
+          
+          calculator.read();
+          alert(calculator.sum());
+          alert(calculator.mul());       
+</script>
